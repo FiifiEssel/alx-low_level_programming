eaf5057c76b9d2d0be3dd063df635fc04e8cedaf
FiifiEssel <ekessel804@gmail.com>
2022-09-15 10:35:22 -0700

    Printing with the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *
+ *  *  *main- Serves as the entry point for the program
+ *
+ *   *   *
+ *
+ *    *    *Return: returns zero if no erros otherwise returns non-zero value.
+ *
+ *     */
+
+int main(void)
+
+{
+
+			puts("\"Programming is like building a multilingual puzzle");
+
+						return (0);
+
+}
