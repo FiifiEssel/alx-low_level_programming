4dbdd7e05bf5ba41dc13fc038a825e9b0d1bac08
FiifiEssel <ekessel804@gmail.com>
2022-09-16 16:18:44 -0700

    Header files for nested loops prject

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,22 @@
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
+/** 
+ * main - Header files for prototypes
+ * void prototype (void)
+ * int prototype (init)
+ */
+
+void print_numbers(void);
+void print_most_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_traingle(int size);
+void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+
+#endif /* MAIN_H */
