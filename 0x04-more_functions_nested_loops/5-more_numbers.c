a07272c3061ea4e6b88975f67e191241a453501b
FiifiEssel <ekessel804@gmail.com>
2022-09-16 18:46:52 -0700

    Printing a set of numbers 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - will print 1 to 14 ten times
+ *
+ * Return: will return void
+ */
+
+void more_numbers(void)
+{
+	int n, n1;
+
+	for (n1 = 1; n1 <= 10; n1++)
+	{
+	for (n - 0; n <= 14; n++)
+	{
+		if (n > 9)
+			putchar((n / 10) + 48));
+	putchar ((n % 10) + 48));
+	}
+	putchar('\n');
+	}
+}
