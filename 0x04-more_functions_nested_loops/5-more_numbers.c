d2f92c2c86c549432b68792017e2dcd3b7244543
FiifiEssel <ekessel804@gmail.com>
2022-09-16 18:50:28 -0700

    Printing numbers for a numberof times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,11 +13,11 @@ void more_numbers(void)
 
 	for (n1 = 1; n1 <= 10; n1++)
 	{
-	for (n - 0; n <= 14; n++)
+	for (n = 0; n <= 14; n++)
 	{
 		if (n > 9)
-			putchar((n / 10) + 48));
-	putchar ((n % 10) + 48));
+			putchar((n / 10) + 48);
+	putchar ((n % 10) + 48);
 	}
 	putchar('\n');
 	}
