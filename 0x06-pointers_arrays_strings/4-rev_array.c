d3681ac542b29c157d82618559d1579a6e7ecb77
FiifiEssel <ekessel804@gmail.com>
2022-09-22 08:55:49 -0700

    Learning and Learning

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -16,9 +16,8 @@ void reverse_array(int *a, int n)
 		for (j = i + 1; j > 0; j--)
 		{
 			temp = *(a + j);
-			*(a + j) = *(a + (j -1));
+			*(a + j) = *(a + (j - 1));
 			*(a + (j - 1)) = temp;
 		}
 	}
-	return (0);
 }
