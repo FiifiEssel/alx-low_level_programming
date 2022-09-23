9d14a88e912f28860ce80f747fc253390cbfffc4
FiifiEssel <ekessel804@gmail.com>
2022-09-23 00:22:19 -0700

    Printing hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
 /**
-* *main-Serves as the entry point for the program
-*
-*/
+ * main-Serves as the entry point for the program
+ *
+ * Return: Always 0
+ *
+ */
 int main(void)
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+
+	return (0);
 }
