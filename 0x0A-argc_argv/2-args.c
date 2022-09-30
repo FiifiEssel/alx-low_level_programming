fb393ec1dc8f233207e294f8c4baea1f92191c17
FiifiEssel <ekessel804@gmail.com>
2022-09-30 15:59:41 -0700

    hello Ags

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
  * main- Where program execution starts
  * @argc: number of command line arguments
  * @argv: an array pointing to argc
  * Return: returns 0 on sucess
  */
-
 int main(int argc, char *argv[])
 {
 	int i;
