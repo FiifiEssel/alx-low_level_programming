92758dbad6cfc5bf64e5a1ce6fab4ab0a6cb208c
FiifiEssel <ekessel804@gmail.com>
2022-09-30 10:24:33 -0700

    learning by doing task 3

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- Where program execution starts
+ * @argc: number of command line arguments
+ * @argv: an array pointing to argc
+ * Return: returns 0 on sucess
+ */
+
+int main(int argc, char *argv[])
+{
+	int 1;
+	for (i = 0; i < argc; i++)
+	{
+		print("%d = %s\n", i, argv[i]);
+	}
+	return (0);
+}
