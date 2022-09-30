8f8c5f1eb6f7b4072250acc05f4be71e7fc68359
FiifiEssel <ekessel804@gmail.com>
2022-09-30 16:23:33 -0700

    my own code

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - where program execution begins
+ * @argc: commands in the command line count
+ * @argv: array storing values of argc
+ * Return: returns 1 when there is an error
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int mul;
+
+	mul = num1 * num2;
+	if (argc != 2)
+	{
+		printf("error \n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", mul);
+	}
+	return (0);
+}
