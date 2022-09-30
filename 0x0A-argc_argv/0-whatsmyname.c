1a0822f30480c8ad05facc58224d317cac3194e1
FiifiEssel <ekessel804@gmail.com>
2022-09-30 09:40:06 -0700

    Arguments project 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main: the start line for program execution
+ * main- the start line for program execution
  * @argc: the count for command line instructions
  * @argv: the string array containing the command line attributes
  * Return: return 0 for sucess
  */
 
-int main(int argc __attibute__((unused)), char *argv[]);
+int main(int argc __attribute__((unused)), char *argv[]);
 {
 	printf("%s\n", argv[0]);
 	return (0);
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,4 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int main(int argc __attibute__((unused)), char *argv[]);
+
 #endif/*MAIN_H*/
