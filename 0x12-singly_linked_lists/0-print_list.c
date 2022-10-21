5dfa4615a91e378298b854964e97e55008833d31
FiifiEssel <ekessel804@gmail.com>
2022-10-21 21:28:46 +0000

    Another push for git linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
-/**
- * print_list - to print all the elements of a list_t list.
- * @h: singly linked list.
- * Return:return number of elements in the list.
- */
 
+/**
+ * print_list - Prints the elements of a list
+ *
+ * @h: pointer to linked list
+ * Return: Returns the calculated number of nodes in the list
+ */
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t num = 0;
 
-	nelem = 0;
-	while (h != NULL)
+	for (; h != NULL; num++)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+		if ((*h).str == NULL)
+		printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		nelem++;
+		printf("[%d] %s\n", (*h).len, (*h).str);
+
+		h = (*h).next;
 	}
-	return (nelem);
+
+	return (num);
 }
